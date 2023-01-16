9ef4eb7250535854665255b95d01b1145486a1bb
chlalswns200 <chlalswns200@naver.com>
2023-01-17 00:56:36 +0900

    first commit

--- src/main/java/com/example/githubactiontest/HelloController.java
+++ src/main/java/com/example/githubactiontest/HelloController.java
@@ -2,8 +2,10 @@ package com.example.githubactiontest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1")
+@RestController
 public class HelloController {
 
     @GetMapping("hello")
