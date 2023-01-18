a77811f5f2dab1efcce7ad57653f59fabad426d6
chlalswns200 <chlalswns200@naver.com>
2023-01-19 00:44:51 +0900

    redis test

--- src/main/java/com/example/githubactiontest/HelloController.java
+++ src/main/java/com/example/githubactiontest/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.githubactiontest;
 
+import com.example.githubactiontest.configuration.RedisConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1")
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final RedisTemplate redisTemplate;
 
     @GetMapping("hello")
     public String hello() {
@@ -14,8 +21,16 @@ public class HelloController {
     }
     @GetMapping("bye")
     public String bye() {
+
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+
+        String key = "token";
+        String value = "1qkwe12jerjnfgrf";
+        valueOperations.set(key,value);
+
         return "action-test-bye!";
     }
+
 
     @GetMapping("bye2")
     public String bye2() {
